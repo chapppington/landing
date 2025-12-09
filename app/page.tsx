c41ea5cec281bc4fd401e6955b993b2364d22ec5
chapppington <stas.mega.96@bk.ru>
2025-12-09 13:19:05 +0700

    (feat) added third screen

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import AboutConferenceScreen from "./screens/AboutConferenceScreen";
 import FirstScreen from "./screens/FirstScreen";
+import SpeakersScreen from "./screens/SpeakersScreen";
 
 export default function Home() {
   return (
     <main>
       <FirstScreen/>
       <AboutConferenceScreen />
+      <SpeakersScreen />
     </main>
   );
 }
